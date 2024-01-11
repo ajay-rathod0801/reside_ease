e8437408849e8572b98503764676018eedf4594d
ajay-rathod0801 <ajay.rathod0801@gmail.comdsgd>
2024-01-11 12:20:17 +0530

    Bottom Navigation Bar with material 3 added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:reside_ease/introductory_screen.dart';
+// import 'package:reside_ease/widgets/bottom_navigation.dart';
 // import 'package:reside_ease/otp_screen.dart';
 // import 'package:reside_ease/login_screen.dart';
 
@@ -20,3 +21,16 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+// uncomment for bottom navigation bar
+// class MyHomePage extends StatelessWidget {
+//   const MyHomePage({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: const IntroductoryScreen(),
+//       bottomNavigationBar: const BottomNavigation(),
+//     );
+//   }
+// }
---
+++ lib/widgets/bottom_navigation.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigation extends StatefulWidget {
+  const BottomNavigation({super.key});
+
+  @override
+  State<BottomNavigation> createState() => _NavigationExampleState();
+}
+
+class _NavigationExampleState extends State<BottomNavigation> {
+  int currentPageIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        backgroundColor: Colors.white,
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.blue.shade200,
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.home),
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          NavigationDestination(
+            icon: Badge(child: Icon(Icons.hive_rounded)),
+            label: 'Community',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.person),
+            icon: Icon(Icons.person_2_outlined),
+            label: 'Messages',
+          ),
+        ],
+      ),
+      body: const Scaffold(
+        body: Center(
+          child: Text('Demo Test'),
+        ),
+      ),
+    );
+  }
+}
